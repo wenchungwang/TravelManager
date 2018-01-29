32d5a93e69a0f26a5d8c0b01ab136c453ac7a146
thomas67.wang <wenchung.wang@gmail.com>
2018-01-29 17:57:49 +0800

    Add page "Main 0" to test

---
+++ app/src/main/java/com/travel_world/travelmanager/Main0Activity.java
@@ -0,0 +1,24 @@
+package com.travel_world.travelmanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main0Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main0);
+    }
+
+    public void click11(View v) {
+
+        Intent it = new Intent(Main0Activity.this, MainActivity.class);
+        startActivity(it);
+
+    }
+
+
+}
